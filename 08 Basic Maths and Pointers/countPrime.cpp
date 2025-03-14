57b4804c37198c0959327a2e440d4563ca4cd56a
Priyansu Singh <priyansusingh3344@gmail.com>
2025-03-14 20:17:38 +0530

    basic maths

--- 08 Basic Maths and Pointers/countPrime.cpp
+++ 08 Basic Maths and Pointers/countPrime.cpp
@@ -1,17 +1,17 @@
 class Solution {
     public:
         int countPrimes(int n) {
-            if( n==0 ) return 0;
-            vector<bool> prime(n,true);
-            prime[0] = prime[1] = false;
-            int ans = 0;
-            for(int i=2; i<n; i++){
-              if(prime[i]){
-                ans++;
-                int j=2*i;
-                while( j<n ){
-                  prime[j] = false;
-                  j+=i;
+            if( n==0 ) return 0; 
+            vector<bool> prime(n,true);  // prime[i] = true means i is prime
+            prime[0] = prime[1] = false; // 0 and 1 are not prime
+            int ans = 0; // number of primes
+            for(int i=2; i<n; i++){ // check if i is prime 
+              if(prime[i]){ // if i is prime, increment ans and mark all multiples of i as not prime
+                ans++; 
+                int j=2*i; // start from 2*i
+                while( j<n ){ // mark all multiples of i as not prime
+                  prime[j] = false; // j is not prime
+                  j+=i; // increment j by i
                 }
               }
             }
--- 08 Basic Maths and Pointers/gcd.cpp
+++ 08 Basic Maths and Pointers/gcd.cpp
@@ -1,19 +1,19 @@
 
 class Solution {
     public:
-      int gcd(int a, int b) {
+      int gcd(int a, int b) { 
           // code here
-          if( a == 0 ) return b;
-          if( b == 0 ) return a;
-          while ( a > 0 && b > 0){
-              if( a>b ){
-                  a = a-b;
+          if( a == 0 ) return b; // if a is 0, return b
+          if( b == 0 ) return a; // if b is 0, return a
+          while ( a > 0 && b > 0){ // while a and b are not 0
+              if( a>b ){ // if a is greater than b, subtract b from a
+                  a = a-b; 
               }
-              else{
+              else{ // if b is greater than a, subtract a from b
                   b = b-a;
               }
           }
-       return a==0? b : a;
+       return a==0? b : a; // return b if a is 0, else return a
       }
   };
   
