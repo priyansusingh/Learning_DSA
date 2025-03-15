1e1212ae4fd0863c89a207f75ae5509dd6d70d33
Priyansu Singh <priyansusingh3344@gmail.com>
2025-03-15 11:37:30 +0530

    optimised seive

--- 08 Basic Maths and Pointers/Optimising Sieve and Segmented Sieve/seiveofEratosthenes.cpp
+++ 08 Basic Maths and Pointers/Optimising Sieve and Segmented Sieve/seiveofEratosthenes.cpp
@@ -25,18 +25,17 @@ vector<bool> optSieve(int n){
     // create a sieve array of N size telling isPrime
     vector<bool> sieve(n+1,true);
     sieve[0]=sieve[1]=false;        
-    for(int i=2; i*i<=n;i++){     //optimization 2-> outer loop
+    for(int i=2; i*i<=n;i++){     //optimization 2-> outer loop , root(n) tak hi jana hai kyuki uske baad ke elements toh pehle hi mark ho chuke honge
         if(sieve[i]==true){
-            int j=i*i;            //optimization 1-> inner loop
+            int j=i*i;            //optimization 1-> inner loop, i*i se start karna hai kyuki i-1 se pehle ke elements toh pehle hi mark ho chuke honge
             while(j<=n){
                 sieve[j]=false;
-                j += i;
+                j += i; //increment by i
             }
         }
     }
     return sieve;
 }
-
 
 int main(){
     // vector<bool>sieve=Sieve(25);
