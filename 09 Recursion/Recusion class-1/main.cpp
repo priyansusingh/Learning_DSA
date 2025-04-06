e496246828d7f80476378626a8e2697ac40c1a76
Priyansu Singh <priyansusingh3344@gmail.com>
2025-04-06 23:46:27 +0530

    recursion

--- 09 Recursion/Recusion class-1/main.cpp
+++ 09 Recursion/Recusion class-1/main.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
 using namespace std;
+ 
+//sum of 1 to n
+int getSum(int n){
+   //base case
+   if(n == 1){
+     return 1;
+   }
+   //recursive relations
+   int ans = n + getSum(n-1);
+   //processing
+   return ans;
+}
+
+//fibonacci series
+int fibo(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+
+    int ans = fibo(n-1) + fibo(n-2);
+
+    return ans;
+}
+
 
 //prints 2^n 
 int pow(int n){
@@ -47,16 +71,18 @@ int main(){
     
     int n;
 
-    cout << "Enter the value of n: ";
-    cin >> n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
 
     // int ans = getFactorial(n);
     // cout << "Factorial of "<< n << " is: " << ans << endl;
 
     // printCounting(5);
 
-    int ans = pow(10);
-    cout << ans <<" ";
-    
+    // int ans = pow(10);
+    // cout << ans <<" ";
+
+    // cout << fibo(9) << endl;
+    cout << getSum(5) << endl;
     return 0;
 }
