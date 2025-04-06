82b49e0544dfbc9fa77dd85b7f765bbf30160eca
Priyansu Singh <priyansusingh3344@gmail.com>
2025-04-06 23:04:30 +0530

    recursion

--- 09 Recursion/Recusion class-1/main.cpp
+++ 09 Recursion/Recusion class-1/main.cpp
@@ -1,6 +1,35 @@
 #include<iostream>
 using namespace std;
 
+//prints 2^n 
+int pow(int n){
+    //base case
+    if( n==0 ){
+        return 1;
+    }
+   
+   //recurrence relation
+   int recKaAns = pow(n-1);
+   int finalAns = 2 * recKaAns;
+
+   //processing
+   return finalAns;
+}
+
+//print counting from n to 1
+void printCounting(int n){
+//base case
+if( n==0 ){
+    return;
+}
+cout << n <<" ";
+//recursive call
+printCounting(n-1);
+//processing
+
+}
+
+
 int getFactorial(int n){
     //base case
     if(n == 0 || n ==1){
@@ -21,8 +50,13 @@ int main(){
     cout << "Enter the value of n: ";
     cin >> n;
 
-    int ans = getFactorial(n);
-    cout << "Factorial of "<< n << " is: " << ans << endl;
+    // int ans = getFactorial(n);
+    // cout << "Factorial of "<< n << " is: " << ans << endl;
 
+    // printCounting(5);
+
+    int ans = pow(10);
+    cout << ans <<" ";
+    
     return 0;
 }
