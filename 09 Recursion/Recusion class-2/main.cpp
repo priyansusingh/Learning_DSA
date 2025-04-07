c563d6051220db142486f4a0d0988dc1da2fa313
Priyansu Singh <priyansusingh3344@gmail.com>
2025-04-07 15:29:01 +0530

    recursion-2

--- 09 Recursion/Recusion class-2/main.cpp
+++ 09 Recursion/Recusion class-2/main.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int main(){ 
+void printArray(int* arr, int size, int index){
+  
+if(index == size){
+   return;
+} 
 
-    
+cout << arr[index] <<" ";
+
+printArray(arr,size,index+1);
+
+}
+
+int main(){ 
+   int arr[] = {12, 32, 24, 21, 43, 53};
+   int size = 6;
+
+   printArray(arr,size, 0);
 
     return 0;
 }
