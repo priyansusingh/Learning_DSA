830ae2e6fd971e16515b75396f6a767ff4467f16
Priyansu Singh <priyansusingh3344@gmail.com>
2025-04-07 15:40:06 +0530

    recursion-2

--- 09 Recursion/Recusion class-2/main.cpp
+++ 09 Recursion/Recusion class-2/main.cpp
@@ -1,14 +1,29 @@
 #include<iostream>
 using namespace std;
 
+//Search in a Array
+int getTheNumber(int* arr, int size, int index, int target){
+    //base case
+    if( arr[index] == target){
+        return index;
+    }
+    //Recursive relation
+    
+    //Processing
+
+}
+
+
+//Print the Array
 void printArray(int* arr, int size, int index){
-  
+//base case
 if(index == size){
    return;
 } 
-
+//Recursive relation
+//ek case mai solve karunga
 cout << arr[index] <<" ";
-
+//baaki recursion sambhal lega
 printArray(arr,size,index+1);
 
 }
