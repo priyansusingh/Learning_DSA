bab04fbca9b2334a4ddb9ebec4af88d5009c162d
Priyansu Singh <priyansusingh3344@gmail.com>
2025-06-17 15:14:00 +0530

    dnc-3

---
+++ 10 Backtracking & DnC/Backtracking & DnC Class-3/leetcode46Permutations.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+    public:
+        
+        void solve(vector<int>& nums, vector<vector<int>> &ans, int i){
+    
+         if(i >= nums.size()){
+            ans.push_back(nums);
+            return;
+         }
+        
+        for(int j=i; j<nums.size(); j++){
+            swap(nums[i],nums[j]);
+            solve(nums,ans,i+1);
+            swap(nums[i],nums[j]);
+        }
+    
+        }
+       
+        vector<vector<int>> permute(vector<int>& nums) {
+            int i=0;
+            vector<vector<int>> ans;
+            solve(nums,ans,i);
+            return ans;
+        }
+    };
