53a92bfcd74a113ad9cc35139e82851650e60acd
Priyansu Singh <priyansusingh3344@gmail.com>
2025-06-25 16:15:13 +0530

    ll-2

--- 12 Linked List/Linked List Class-2/main.cpp
+++ 12 Linked List/Linked List Class-2/main.cpp
@@ -15,6 +15,16 @@ class Node{
 
 };
 
+int getLength(Node* &head){
+   int len = 0;
+   Node* temp = head;
+   while(temp != NULL){
+    len++;
+    temp = temp->next;
+   }
+   return len;
+}
+
 void insertAtHead(int value, Node* &head, Node* &tail){
     
     if( head == NULL && tail == NULL){
@@ -41,6 +51,49 @@ void print(Node* head){
 
 }
 
+void insertAtTail(int value,Node* &head, Node* &tail){
+    if( head == NULL && tail == NULL ){
+        Node* newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+   else{
+    Node* newNode = new Node(value);
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+   }
+}
+
+void insertAtPosition(int value, Node* &head, Node* &tail, int position){
+    int len = getLength(head);
+    if(position < 1 || position > len + 1){
+        cout << "Invalid position!" << endl;
+        return;
+    }
+    if( position == 1){
+        insertAtHead(value,head,tail);
+    }
+    else if(position == len+1){
+        insertAtTail(value,head,tail);
+    
+    }
+else{
+    Node* temp = head;
+    for(int i=0; i<position-2; i++){
+       temp = temp->next;
+    }
+  
+    Node* newNode = new Node(value);
+    Node* forward = temp->next;
+    newNode->prev = temp;
+    temp->next = newNode;
+    forward->prev = newNode;
+    newNode->next = forward;
+}
+}
+
 void printReverse(Node* tail){
     Node* temp = tail;
     while(temp != NULL){
@@ -64,5 +117,11 @@ int main(){
    insertAtHead(50,head,tail);
    print(head);
    printReverse(tail);
+   insertAtTail(500,head,tail);
+   print(head);
+   insertAtPosition(300,head,tail,7);
+   print(head);
+   cout << "Length: " << getLength(head) << endl;
+//    printReverse(tail);
     return 0;
 }
