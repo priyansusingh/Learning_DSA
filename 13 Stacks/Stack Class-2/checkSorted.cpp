7491ae4b403d2f9132a7926690dca6b15ed28b44
Priyansu Singh <priyansusingh3344@gmail.com>
2025-06-29 10:26:39 +0530

    stacl-2

---
+++ 13 Stacks/Stack Class-2/checkSorted.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<stack>
+#include<limits.h>
+using namespace std;
+
+bool checkSorted(stack<int> &s, int element1){
+    if(s.empty()){
+        //iska matlab stack pura check ho chucka h, aur sorted h
+        return true;
+    }
+
+    int element2 = s.top();
+    s.pop();
+    
+    if(element2 < element1){
+        bool aageKaAns = checkSorted(s,element2);
+        s.push(element2);
+        return aageKaAns;
+    }
+    else{
+        return false;
+    }
+}
+
+int main() {
+  stack<int> s;
+  s.push(10);
+  s.push(20);
+  s.push(90);
+  s.push(40);
+  s.push(50);
+
+  int element1 = INT_MAX;
+
+  cout << checkSorted(s,element1) <<endl;
+  return 0;
+}
---
+++ 13 Stacks/Stack Class-2/reverseStack.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+
+void insertAtBottom( stack<int> &s, int value){
+   if(s.empty()){
+    s.push(value);
+    return;
+   }
+
+   int topELement = s.top();
+   s.pop();
+   insertAtBottom(s,value);
+   s.push(topELement);
+}
+
+void reverseStack( stack<int> &s){
+    if(s.empty()){
+      return;
+    }
+    int topElement = s.top();
+    s.pop();
+    reverseStack(s);
+    insertAtBottom(s,topElement);
+}
+
+int main() {
+  stack<int> s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  s.push(40);
+  s.push(50);
+  reverseStack(s);
+  
+  while(!s.empty()){
+    cout << s.top() <<" ";
+    s.pop();
+  }
+
+
+  return 0;
+}
