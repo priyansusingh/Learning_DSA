bce5daf971e2c9dc51134bda09dd66d9db206caf
Priyansu Singh <priyansusingh3344@gmail.com>
2025-08-27 12:38:08 +0530

    changes

--- 07 Char Arrays And Strings/Char Arrays & Strings Class-1/convertIntoLowerOrUpperCase.cpp
+++ 07 Char Arrays And Strings/Char Arrays & Strings Class-1/convertIntoLowerOrUpperCase.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int getLength(char arr[], int size){
     int count =0;
     int index = 0;
-    while(arr[index]!= '\0'){
+    while(arr[index] != '\0'){
         //till null character is not found
         count++;
         index++;
